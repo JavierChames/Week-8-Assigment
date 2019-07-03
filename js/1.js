2bfad6779409bbc0ce11c73338e2212d9c9c09b1
Javier Chames <haimch@gmail.com>
2019-07-03 14:47:23 +0300

    Added features
    each new task added to list with checkbox & icno to delete
    added limit of 50 chars to input box

--- js/1.js
+++ js/1.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => { //Wait till page finally loaded
 
+    class Dolist {
+        constructor() {
+            name
+            checked
+
+            alert("hi");
+        }
+    }
+
+    let taskId=0;
+
     function GetTextFromUser() { //Check if appear text in Textbox
         if ($('#taskText').val()) {
             let taskName = $('#taskText').val();
@@ -8,21 +19,38 @@ document.addEventListener("DOMContentLoaded", () => { //Wait till page finally l
     }
 
     $("#addTask").click(function () { //Added listener to addButton
-        if (GetTextFromUser()){
-        let myTxt=GetTextFromUser();
-        // addItemToList(myTxt);
-        $('#taskText').val("");
-        
+        if (GetTextFromUser()) {
+            let myTxt = GetTextFromUser();
+            addItemToList(myTxt);
+            $('#taskText').val("");
         }
     });
 
     $("#taskText").keyup(function (e) { //If enter pressed
-        if (e.keyCode == 13 && GetTextFromUser()) {
-            let myTxt=GetTextFromUser();
-            // addItemToList(myTxt);
+        if (e.keyCode == 13 && GetTextFromUser()) { //Check if enter pressed & txt is not empty
+            let myTxt = GetTextFromUser();
+            addItemToList(myTxt);
             $('#taskText').val("");
         }
     });
+
+    function addItemToList(myTxt) {
+        taskId++;
+
+        let ul = document.getElementById("mainList");
+        let listItem = document.createElement("li");
+        let checkBoxItem=document.createElement("input");
+        let trashIcon=document.createElement("img");
+
+        checkBoxItem.setAttribute("type","checkbox");
+        listItem.setAttribute("id",`taskID ${taskId}`); //String Literal
+        trashIcon.setAttribute("src","../images/trash.png"); 
+        trashIcon.setAttribute("align","right"); 
+        listItem.appendChild(checkBoxItem);
+        listItem.appendChild(document.createTextNode(` ${myTxt}` ));
+        listItem.appendChild(trashIcon);
+        ul.appendChild(listItem);
+    }
 
 
 });
